437395cb24ad0d2c3e7045387d6c65babc99b82f
nuKaross <127345332+nuKaross@users.noreply.github.com>
2023-04-15 19:07:39 +0200

    solved 8 exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR";
+    if (start < 0 || end < 0) return 'ERROR';
+    let sum = 0;
 
+   if (end>start)
+    for (let i = start;i<=end;i++){
+        sum+=i;
+    }
+    else if (end<start){
+        for (let i = end;i<=start;i++){
+            sum+=i;
+    }
 };
-
+    return sum;
+}
 // Do not edit below this line
 module.exports = sumAll;
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(x,y) {
+    return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x,y) {
+    return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+   let sum = 0;
+  array.forEach(element => {
+      sum += element;
+    });
+    return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+    let total = array[0];
+    for(let i=1; i<array.length; i++){
+      total *= array[i];
+    }
+    return total;
+  };
+  
 
+const power = function(x,y) {
+    return Math.pow(x,y);
 };
 
-const power = function() {
-	
-};
+const factorial = function(n) {
+  if (n==0) return 1;
+    let product = 1;
+    for (let i = n; i>0;i--){
+      product *= i;
+    }
+    return product;
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
